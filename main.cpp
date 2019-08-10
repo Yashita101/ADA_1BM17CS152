1fe19291eaace7ccd819c86b14f95a2116f639d8
Yashita101 <53893563+Yashita101@users.noreply.github.com>
2019-08-10 19:51:55 +0530

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,19 +1,41 @@
 #include <iostream>
+#include <fstream>
+
 using namespace std;
 
 int main()
 {
-	int i, n, max=0;
-	cout<<"Enter the number of integers:";
-	cin>>n;
-	int a[n];
-	cout<<"Enter the integer values:";
-	for(i=0;i<n;i++)
-		cin>>a[i];
-	for(i=0;i<n;i++)
-	{if(a[i]>max)
-		max=a[i];
-	}
-	cout<<"MAXIMUM"<<max;
-	return 0;
+    ifstream in("C:/Users/Dell/Desktop/search.txt",std::ios_base::in);
+    int i,a[50],j,key,n,test_cases,flag;
+    if(!in)
+    {
+        cout<<"Error in opening the file.";
+        return 1;
+    }
+    while(in)
+    {
+        //cout<<"Enter the number of test cases.";
+        in>>test_cases;
+        //cout<<"Enter the number of elements and the key value.";
+        for(i=0;i<test_cases;i++)
+        {
+            in>>n;
+            in>>key;
+          //  cout<<"Enter the array elements.";
+            for(j=0;j<n;j++)
+            {
+                in>>a[j];
+                if(a[j]==key)
+                    flag=1;
+                else
+                    flag=0;
+            }
+        }
+        if(flag==1)
+            cout<<"1"<<"\n"<<endl;
+        else if(flag==0)
+            cout<<"-1"<<"\n";
+
+    }
+    return 0;
 }
