dd8570f4d20f3c2cd414cfab6ab8f6a133295f91
Yashita101 <53893563+Yashita101@users.noreply.github.com>
2019-08-10 22:37:16 +0530

    Add files via upload

--- search.cpp
+++ search.cpp
@@ -1,41 +1,37 @@
-#include <iostream>
+#include<iostream>
 #include <fstream>
+#include <string>
 
 using namespace std;
 
 int search()
 {
-    ifstream in("C:/Users/Dell/Desktop/search.txt",std::ios_base::in);
-    int i,a[50],j,key,n,test_cases,flag;
-    if(!in)
-    {
-        cout<<"Error in opening the file.";
-        return 1;
-    }
-    while(in)
-    {
-        //cout<<"Enter the number of test cases.";
-        in>>test_cases;
-        //cout<<"Enter the number of elements and the key value.";
-        for(i=0;i<test_cases;i++)
-        {
-            in>>n;
-            in>>key;
-          //  cout<<"Enter the array elements.";
-            for(j=0;j<n;j++)
-            {
-                in>>a[j];
-                if(a[j]==key)
-                    flag=1;
-                else
-                    flag=0;
-            }
-        }
-        if(flag==1)
-            cout<<"1"<<"\n"<<endl;
-        else if(flag==0)
-            cout<<"-1"<<"\n";
+	string num;
+    std::ifstream myfile("C:/Users/Dell/Desktop/search.txt", std::ios_base::in);
+	myfile>>num;
+	int t,n,i,j,k,flag=-1,a[50];
+	t=atoi(num.c_str());
 
-    }
-    return 0;
+	for(i=0;i<t;i++)
+	{
+		myfile>>num;
+		n=atoi(num.c_str());
+		myfile>>num;
+        k=atoi(num.c_str());
+
+		for(j=0;j<n;j++)
+		{
+			myfile >> num;
+			a[i]=atoi(num.c_str());
+
+		}
+		if(a[i]==k)
+            flag=1;
+        else
+            flag=-1;
+
+		cout<<flag<<"\n";
+
+	}
 }
+
